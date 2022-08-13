ddbdaedb9674d94d839bc7f38270279e891cabc0
nasim alizai <sayedhakimi449@gmail.com>
2022-08-13 08:34:15 +0430

    chnages

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,19 @@
-
 var clock;
-		
-		$(document).ready(function() {
-			var clock;
 
-			clock = $('.clock').FlipClock({
-		        clockFace: 'DailyCounter',
-		        autoStart: false,
-		        callbacks: {
-		        	stop: function() {
-		        		$('.message').html('The clock has stopped!')
-		        	}
-		        }
-		    });
-				    
-		    clock.setTime(220880);
-		    clock.setCountdown(true);
-		    clock.start();
+$(document).ready(function () {
+	var clock;
 
-		});
+	clock = $(".clock").FlipClock({
+		clockFace: "DailyCounter",
+		autoStart: false,
+		callbacks: {
+			stop: function () {
+				$(".message").html("The clock has stopped!");
+			},
+		},
+	});
+
+	clock.setTime(220880);
+	clock.setCountdown(true);
+	clock.start();
+});
